0233dcde8edabab41e0623051dd48a3e73145af4
meriendaa <a19teomerrod@inspedralbes.cat>
2023-03-12 21:46:15 +0100

    fin

--- server.js
+++ server.js
@@ -1,34 +1,52 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const app = express();
-const http = require('http');
+const http = require("http");
 const { Server } = require("socket.io");
 const server = http.createServer(app);
-const { Socket } = require('dgram');
-const CryptoJS = require('crypto-js');
+const { Socket } = require("dgram");
+const CryptoJS = require("crypto-js");
 
+const io = new Server(server);
 
 const PORT = 7080;
+
 app.use(express.json());
 
-app.use(cors({
-    credentials: true,
-    origin: function (origin, callback) {
-        return callback(null, true)
-    }
-}));
+app.use(
+    cors({
+        credentials: true,
+        origin: function (origin, callback) {
+            return callback(null, true);
+        },
+    })
+);
 
 
-app.listen(PORT, () => {
-    console.log("Server Running [" + PORT + "]");
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-
-
 app.post("/hash", (req, res) => {
-	console.log(req.body.values[0]) //aqui recibimos el texto introducido en la caja de texto.
-	let txt_hash = CryptoJS.SHA256(req.body.values[0]).toString();
-    res.send(JSON.stringify(txt_hash)); //enviamos al js del cliente el texto hasheado
+    let result = CryptoJS.SHA256(req.body.values[0]).toString();
+    res.send(JSON.stringify(result)); //enviamos al js del cliente el texto hasheado
+});
+
+//Controlar les connexions i desconnexions
+io.on('connection', (socket) => {
+    console.log(`Cliente conectado`);
+    
+    socket.on('mensaje', (mensaje) => {
+      console.log(`Mensaje recibido: ${mensaje}`);
+      
+      // Envía el mensaje a todos los clientes conectados
+      io.emit('mensaje', mensaje);
+    });
+  });
+  
+
+server.listen(PORT, () => {
+    console.log("Sockets Running [" + PORT + "]");
 });
